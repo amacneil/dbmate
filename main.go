7dbcffc352c99d27dbed41b3a6a833e7fe828e65
Reuben Thomas-Davis <reubenestd@gmail.com>
2019-12-31 20:47:27 +0000

    :sparkles: Add --wait flag to CLI

--- main.go
+++ main.go
@@ -49,6 +49,10 @@ func NewApp() *cli.App {
 			Name:  "no-dump-schema",
 			Usage: "don't update the schema file on migrate/rollback",
 		},
+		cli.BoolFlag{
+			Name: "wait",
+			Usage: "wait for the db to become available before executing the subsequent command",
+		},
 	}
 
 	app.Commands = []cli.Command{
@@ -138,6 +142,7 @@ func action(f func(*dbmate.DB, *cli.Context) error) cli.ActionFunc {
 		db.AutoDumpSchema = !c.GlobalBool("no-dump-schema")
 		db.MigrationsDir = c.GlobalString("migrations-dir")
 		db.SchemaFile = c.GlobalString("schema-file")
+		db.WaitBefore = c.GlobalBool("wait")
 
 		return f(db, c)
 	}
