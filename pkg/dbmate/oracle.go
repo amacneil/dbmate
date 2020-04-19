7b01a67ea8dada5162060ef3871ae40b4da26b95
Carlo Suriano <carlo.suriano@deustechnology.com>
2020-04-19 10:36:11 +0200

    Add oracle driver implementation (schema dump missing)

--- pkg/dbmate/oracle.go
+++ pkg/dbmate/oracle.go
@@ -1,14 +1,10 @@
 package dbmate
 
 import (
-	"bytes"
 	"database/sql"
 	"fmt"
-	"github.com/lib/pq"
 	"net/url"
 	"strings"
-
-	"gopkg.in/rana/ora.v4"
 )
 
 func init() {
@@ -19,29 +15,32 @@ func init() {
 type OracleDriver struct {
 }
 
-// Open creates a new database connection
+// Open creates a new database connection. In oracle connecting to a database means connecting to a user
+// which is also a schema
 func (drv OracleDriver) Open(u *url.URL) (*sql.DB, error) {
 	return sql.Open("oracle", u.String())
 }
 
 func (drv OracleDriver) openOracleDB(u *url.URL) (*sql.DB, error) {
 	// connect to postgres database
-	postgresURL := *u
-	postgresURL.Path = "oracle"
+	oracleURL := *u
+	oracleURL.Path = "oracle"
 
-	return drv.Open(&postgresURL)
+	return drv.Open(&oracleURL)
 }
 
 func oracleQuoteIdentifier(str string) string {
-	str = strings.Replace(str, "`", "\\`", -1)
+	str = strings.Replace(str, "\"", "\\\"", -1)
 
-	return fmt.Sprintf("`%s`", str)
+	return fmt.Sprintf("\"%s\"", str)
 }
 
-// CreateDatabase creates the specified database
+// CreateDatabase creates a new user/schema and assigns connection, create session and granting privileges
+// this requires that the creating user, as specified in URL, has `create user` privilege
 func (drv OracleDriver) CreateDatabase(u *url.URL) error {
-	name := databaseName(u)
-	fmt.Printf("Creating: %s\n", name)
+	name := u.User.Username()
+	password, _ := u.User.Password()
+	fmt.Printf("Creating schema: %s\n", name)
 
 	db, err := drv.openOracleDB(u)
 	if err != nil {
@@ -49,80 +48,60 @@ func (drv OracleDriver) CreateDatabase(u *url.URL) error {
 	}
 	defer mustClose(db)
 
-	_, err = db.Exec(fmt.Sprintf("create database %s",
-		oracleQuoteIdentifier(name)))
+	_, err = db.Exec(fmt.Sprintf("create user %s identified by %s;", name, password))
+
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(fmt.Sprintf("grant connect to %s;", name))
+
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(fmt.Sprintf("grant create session grant any privilege to %s;", name))
 
 	return err
 }
 
-// DropDatabase drops the specified database (if it exists)
-func (drv PostgresDriver) DropDatabase(u *url.URL) error {
-	name := databaseName(u)
+// DropDatabase drops the specified user/schema and all objects contained in it
+func (drv OracleDriver) DropDatabase(u *url.URL) error {
+	name := u.User.Username()
 	fmt.Printf("Dropping: %s\n", name)
 
-	db, err := drv.openPostgresDB(u)
+	db, err := drv.openOracleDB(u)
 	if err != nil {
 		return err
 	}
 	defer mustClose(db)
 
-	_, err = db.Exec(fmt.Sprintf("drop database if exists %s",
-		pq.QuoteIdentifier(name)))
+	_, err = db.Exec(fmt.Sprintf("drop user %s cascade", name))
 
 	return err
 }
 
-func postgresSchemaMigrationsDump(db *sql.DB) ([]byte, error) {
-	// load applied migrations
-	migrations, err := queryColumn(db,
-		"select quote_literal(version) from public.schema_migrations order by version asc")
-	if err != nil {
-		return nil, err
-	}
-
-	// build schema_migrations table data
-	var buf bytes.Buffer
-	buf.WriteString("\n--\n-- Dbmate schema migrations\n--\n\n")
-
-	if len(migrations) > 0 {
-		buf.WriteString("INSERT INTO public.schema_migrations (version) VALUES\n    (" +
-			strings.Join(migrations, "),\n    (") +
-			");\n")
-	}
-
-	return buf.Bytes(), nil
-}
-
 // DumpSchema returns the current database schema
-func (drv PostgresDriver) DumpSchema(u *url.URL, db *sql.DB) ([]byte, error) {
-	// load schema
-	schema, err := runCommand("pg_dump", "--format=plain", "--encoding=UTF8",
-		"--schema-only", "--no-privileges", "--no-owner", u.String())
-	if err != nil {
-		return nil, err
-	}
+func (drv OracleDriver) DumpSchema(u *url.URL, db *sql.DB) ([]byte, error) {
+	/* TODO reveng schema
+	https://stackoverflow.com/questions/33704685/dumping-a-complete-oracle-11g-database-schema-to-a-set-of-sql-creation-statement
+	http://www.orafaq.com/node/807
 
-	migrations, err := postgresSchemaMigrationsDump(db)
-	if err != nil {
-		return nil, err
-	}
-
-	schema = append(schema, migrations...)
-	return trimLeadingSQLComments(schema)
+	refer to `dbms_metadata.get_ddl`, `dbms_metadata.get_dependent_ddl`, `all_dependencies`, `all_constraints`
+	*/
+	return nil, nil
 }
 
 // DatabaseExists determines whether the database exists
-func (drv PostgresDriver) DatabaseExists(u *url.URL) (bool, error) {
-	name := databaseName(u)
-
-	db, err := drv.openPostgresDB(u)
+func (drv OracleDriver) DatabaseExists(u *url.URL) (bool, error) {
+	db, err := drv.openOracleDB(u)
 	if err != nil {
 		return false, err
 	}
 	defer mustClose(db)
 
 	exists := false
-	err = db.QueryRow("select true from pg_database where datname = $1", name).
+	err = db.QueryRow("select 1 from dual").
 		Scan(&exists)
 	if err == sql.ErrNoRows {
 		return false, nil
@@ -132,19 +111,24 @@ func (drv PostgresDriver) DatabaseExists(u *url.URL) (bool, error) {
 }
 
 // CreateMigrationsTable creates the schema_migrations table
-func (drv PostgresDriver) CreateMigrationsTable(db *sql.DB) error {
-	_, err := db.Exec("create table if not exists public.schema_migrations " +
-		"(version varchar(255) primary key)")
+func (drv OracleDriver) CreateMigrationsTable(db *sql.DB) error {
+	_, err := db.Exec("create table schema_migrations " +
+		"(version varchar2(255), primary key(version))")
 
 	return err
 }
 
 // SelectMigrations returns a list of applied migrations
 // with an optional limit (in descending order)
-func (drv PostgresDriver) SelectMigrations(db *sql.DB, limit int) (map[string]bool, error) {
-	query := "select version from public.schema_migrations order by version desc"
+func (drv OracleDriver) SelectMigrations(db *sql.DB, limit int) (map[string]bool, error) {
+	var query string
+	baseQuery := "select version from schema_migrations"
+	orderClause := "order by version desc"
+
 	if limit >= 0 {
-		query = fmt.Sprintf("%s limit %d", query, limit)
+		query = fmt.Sprintf("%s where rownum < %d %s", baseQuery, limit, orderClause)
+	} else {
+		query = fmt.Sprintf("%s %s", baseQuery, orderClause)
 	}
 	rows, err := db.Query(query)
 	if err != nil {
@@ -167,25 +151,22 @@ func (drv PostgresDriver) SelectMigrations(db *sql.DB, limit int) (map[string]bo
 }
 
 // InsertMigration adds a new migration record
-func (drv PostgresDriver) InsertMigration(db Transaction, version string) error {
-	_, err := db.Exec("insert into public.schema_migrations (version) values ($1)", version)
+func (drv OracleDriver) InsertMigration(db Transaction, version string) error {
+	_, err := db.Exec("insert into schema_migrations (version) values ($1)", version)
 
 	return err
 }
 
 // DeleteMigration removes a migration record
-func (drv PostgresDriver) DeleteMigration(db Transaction, version string) error {
-	_, err := db.Exec("delete from public.schema_migrations where version = $1", version)
+func (drv OracleDriver) DeleteMigration(db Transaction, version string) error {
+	_, err := db.Exec("delete from schema_migrations where version = $1", version)
 
 	return err
 }
 
 // Ping verifies a connection to the database server. It does not verify whether the
 // specified database exists.
-func (drv PostgresDriver) Ping(u *url.URL) error {
-	// attempt connection to primary database, not "postgres" database
-	// to support servers with no "postgres" database
-	// (see https://github.com/amacneil/dbmate/issues/78)
+func (drv OracleDriver) Ping(u *url.URL) error {
 	db, err := drv.Open(u)
 	if err != nil {
 		return err
@@ -194,12 +175,6 @@ func (drv PostgresDriver) Ping(u *url.URL) error {
 
 	err = db.Ping()
 	if err == nil {
-		return nil
-	}
-
-	// ignore 'database "foo" does not exist' error
-	pqErr, ok := err.(*pq.Error)
-	if ok && pqErr.Code == "3D000" {
 		return nil
 	}
 
