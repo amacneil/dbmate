afbad756c57d7facfa9e772a33136e06f6d6f675
Adrian Macneil <adrian@adrianmacneil.com>
2020-10-31 14:54:04 +1300

    Add queryRow function to get single value

--- pkg/dbmate/driver.go
+++ pkg/dbmate/driver.go
@@ -30,6 +30,8 @@ func RegisterDriver(drv Driver, scheme string) {
 // Transaction can represent a database or open transaction
 type Transaction interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
+	Query(query string, args ...interface{}) (*sql.Rows, error)
+	QueryRow(query string, args ...interface{}) *sql.Row
 }
 
 // GetDriver loads a database driver by name
