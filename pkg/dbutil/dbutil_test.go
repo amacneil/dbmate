d93e2c528e540f54d69470d634a134fa8326995f
Adrian Macneil <adrian@adrianmacneil.com>
2020-11-19 14:52:56 +1300

    Use in-memory sqlite database for testing dbutil

--- pkg/dbutil/dbutil_test.go
+++ pkg/dbutil/dbutil_test.go
@@ -2,12 +2,11 @@ package dbutil_test
 
 import (
 	"database/sql"
-	"os"
 	"testing"
 
 	"github.com/amacneil/dbmate/pkg/dbutil"
-	"github.com/amacneil/dbmate/pkg/driver/sqlite"
 
+	_ "github.com/mattn/go-sqlite3" // database/sql driver
 	"github.com/stretchr/testify/require"
 )
 
@@ -36,19 +35,21 @@ func TestTrimLeadingSQLComments(t *testing.T) {
 	require.Equal(t, "real stuff\n-- end\n", string(out))
 }
 
+// connect to in-memory sqlite database for testing
+const sqliteMemoryDB = "file:dbutil.sqlite3?mode=memory&cache=shared"
+
 func TestQueryColumn(t *testing.T) {
-	u := dbutil.MustParseURL(os.Getenv("SQLITE_TEST_URL"))
-	db, err := sql.Open("sqlite3", sqlite.ConnectionString(u))
+	db, err := sql.Open("sqlite3", sqliteMemoryDB)
 	require.NoError(t, err)
 
-	val, err := dbutil.QueryColumn(db, "select 'foo_' || atom from json_each(?)", `["hi", "there"]`)
+	val, err := dbutil.QueryColumn(db, "select 'foo_' || val from (select ? as val union select ?)",
+		"hi", "there")
 	require.NoError(t, err)
 	require.Equal(t, []string{"foo_hi", "foo_there"}, val)
 }
 
 func TestQueryValue(t *testing.T) {
-	u := dbutil.MustParseURL(os.Getenv("SQLITE_TEST_URL"))
-	db, err := sql.Open("sqlite3", sqlite.ConnectionString(u))
+	db, err := sql.Open("sqlite3", sqliteMemoryDB)
 	require.NoError(t, err)
 
 	val, err := dbutil.QueryValue(db, "select $1 + $2", "5", 2)
