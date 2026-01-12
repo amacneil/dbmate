9f3287e66c4a1ce854e00ccc2687955ef96c8e56
Adrian Macneil <adrian@foxglove.dev>
2026-01-11 19:38:47 -0800

    Remove unnecessary NODE_AUTH_TOKEN line

--- typescript/publish.ts
+++ typescript/publish.ts
@@ -8,9 +8,6 @@ async function main() {
   );
 
   for (const pkg of packages) {
-    // Unset NODE_AUTH_TOKEN to avoid conflicts with OIDC trusted publishing
-    delete process.env.NODE_AUTH_TOKEN;
-    await exec("corepack", ["npm", "--version"]);
     await exec("corepack", [
       "npm",
       "publish",
